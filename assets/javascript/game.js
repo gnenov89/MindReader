1d59925e53f2c3cfacac69160d1851ab82a4c116
Georgi Nenov <georginenov@Georgis-MacBook-Pro.local>
2017-07-11 10:48:39 -0400

    FinalTouch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,44 @@
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var answer = letters[Math.floor(Math.random() *25 + 1)];
+var answer = letters[Math.floor(Math.random() * 25 + 1)];
 var wins = 0;
 var losses = 0;
-var GuessesLeft = 9;
-var guesses = []; 
+var guessesLeft = 9;
+var guesses = [];
 var winsHTML = document.getElementById("wins");
 var lossesHTML = document.getElementById("losses");
-var GuessesLeftHTML= document.getElementById("guessesLeft");
+var guessesLeftHTML = document.getElementById("guessesLeft")
 var guessesHTML = document.getElementById("guesses");
-console.log("The answer is:" + answer+ ".");
+console.log("Answer Key: The answer is " + answer + ".");
 window.addEventListener("keyup", (guess) => {
-	if (GuessesLeft <= 0) {
-		losses ++;
-       alert("Game over!!!No more guesses aloowed!Wanna try again?");
-       answer = letters[Math.floor(Math.random() * 25 + 1)];
-       guessesLeft = 9;
-       console.log("The answer is" + answer + ".");
-
-	} else {
+  if (guessesLeft <= 0) {
+    losses ++;
+    lossesHTML.innerHTML = losses;
+    alert("Negative, Captain! I encourage you to try again!");
+    answer = letters[Math.floor(Math.random() * 25 + 1)];
+    guessesLeft = 9;
+    console.log("Answer Key: The new answer is " + answer + ".");
+    guessesLeftHTML.innerHTML = "10";
+    guesses = [];
+    guessesHTML.innerHTML = "None"
+  } else {
       if (guess.key < "a" || guess.key > "z") {
-        alert("You know that's not a letter, right? Try again, and guess a letter (in lower case) this time!");
+        alert("Not a letter! We do the letter game - no numbers!");
       } else if (guess.key.toUpperCase() === answer){
           wins ++;
-          alert("RIGHT! How did you know I was thinking of the letter " + guess.key.toUpperCase() + "? Let's play again! Try to guess the new letter I'm thinking of now.")
+          winsHTML.innerHTML = wins;
+          alert("Big Success, " + guess.key.toUpperCase() + ", high five!.")
           answer = letters[Math.floor(Math.random() * 25 + 1)];
           guessesLeft = 9;
-          console.log("The new answer is " + answer + ".");
+          console.log("Answer Key: The new answer is " + answer + ".");
+          guessesLeftHTML.innerHTML = "10";
+          guesses = [];
+          guessesHTML.innerHTML = "None"
         } else {
             guessesLeft --;
-            alert("WRONG! I'm not thinking of the letter " + guess.key.toUpperCase() + ". Keep trying!");
+            guesses.push(guess.key.toUpperCase());
+            guessesHTML.innerHTML = guesses.join(" ");
+            guessesLeftHTML.innerHTML = (guessesLeft + 1);
+            alert("No more  " + guess.key.toUpperCase() + ", attempts. It's been a quite a journey!");
           }
     }
 });
-
-
